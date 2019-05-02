8d57e5d36ffca931ebd5a47abdfff06599bc46b7
Shibani S <css7@jci.com>
2019-05-02 17:47:17 +0530

    Example chart added - for statistics with dummy data

--- src/app/show/chart-statistics/chart-statistics.component.ts
+++ src/app/show/chart-statistics/chart-statistics.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
 
 @Component({
   selector: 'app-chart-statistics',
@@ -7,9 +18,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartStatisticsComponent implements OnInit {
 
+  public options: any =  {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares in January, 2018'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: 'red' || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        type:'pie',
+        data: []
+        // data: [{
+        //     name: 'Chrome',
+        //     y: 61.41,
+        //     sliced: true,
+        //     selected: true
+        // }, {
+        //     name: 'Internet Explorer',
+        //     y: 11.84
+        // }, {
+        //     name: 'Firefox',
+        //     y: 10.85
+        // }, {
+        //     name: 'Edge',
+        //     y: 4.67
+        // }, {
+        //     name: 'Safari',
+        //     y: 4.18
+        // }, {
+        //     name: 'Sogou Explorer',
+        //     y: 1.64
+        // }, {
+        //     name: 'Opera',
+        //     y: 1.6
+        // }, {
+        //     name: 'QQ',
+        //     y: 1.2
+        // }, {
+        //     name: 'Other',
+        //     y: 2.61
+        // }]
+    }]
+}
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
+
+    
+
   }
+    
 
 }
--- src/app/show/shared/pipe/remove-spaces.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RemoveSpacesPipe } from './remove-spaces.pipe';
-
-describe('RemoveSpacesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RemoveSpacesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
